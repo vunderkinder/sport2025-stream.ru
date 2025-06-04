b8617165744adfe98a7134b30e1cac8abc289145
vunderkinder <131601657+vunderkinder@users.noreply.github.com>
2025-06-04 23:31:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// Простой скрипт для вставки URL трансляции
+document.addEventListener("DOMContentLoaded", () => {
+    const player = document.getElementById("streamPlayer");
+    // Вставьте сюда ссылку на трансляцию
+    player.src = "https://www.example.com/stream";
+});
